e24ff4317ae348e4e310a3d7139b4dab36c9a680
rosey-mccloskey-ch <rmccloskey@companieshouse.gov.uk>
2025-08-12 11:56:16 +0100

    SIV-537: Consumer Skeleton

---
+++ src/main/java/uk/gov/companieshouse/authcodeChanged/AuthcodeChangedConsumerApplication.java
@@ -0,0 +1,11 @@
+package uk.gov.companieshouse.authcodeChanged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcodeChangedConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthcodeChangedConsumerApplication.class, args);
+    }
+}
