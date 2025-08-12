d23ca5a330977e620fe90937899a97b3bd466b6f
rbromfield-ch <rbromfield1@companieshouse.gov.uk>
2025-08-12 12:12:31 +0100

    SIV-537: Add placeholder test

---
+++ src/test/java/uk/gov/companieshouse/authcodeChanged/AuthcodeChangedConsumerApplicationTest.java
@@ -0,0 +1,19 @@
+package uk.gov.companieshouse.authcodeChanged;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.Test;
+
+public class AuthcodeChangedConsumerApplicationTest {
+  // This is a placeholder for the test class.
+    @Test
+    public void testMain() {
+        String[] args = {};
+        AuthcodeChangedConsumerApplication.main(args);
+        // Add assertions or verifications as needed
+        assertTrue(true); // Placeholder assertion
+    }
+
+
+}
