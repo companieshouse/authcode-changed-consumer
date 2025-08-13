4e58fa677cef76f2566a04522f2f0214f118a6a8
rosey-mccloskey-ch <rmccloskey@companieshouse.gov.uk>
2025-08-13 09:42:40 +0100

    SIV-537: amending application.properties AuthcodeChangedConsumerApplication and test
    # Conflicts:
    #	src/test/java/uk/gov/companieshouse/authcodeChanged/AuthcodeChangedConsumerApplicationTest.java

--- src/test/java/uk/gov/companieshouse/authcodeChanged/AuthcodeChangedConsumerApplicationTest.java
+++ src/test/java/uk/gov/companieshouse/authcodeChanged/AuthcodeChangedConsumerApplicationTest.java
@@ -2,16 +2,15 @@ package uk.gov.companieshouse.authcodeChanged;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.google.common.annotations.VisibleForTesting;
 import org.junit.Test;
 
 public class AuthcodeChangedConsumerApplicationTest {
-    // This is a placeholder test
-    // Please replace with actual tests when functionality is implemented
+  // TODO: This is a placeholder for the test class. replace with actual tests when functionality is implemented
     @Test
     public void testMain() {
         String[] args = {};
         AuthcodeChangedConsumerApplication.main(args);
-        assertTrue(true);
+        // Add assertions or verifications as needed
+        assertTrue(true); // Placeholder assertion
     }
 }
