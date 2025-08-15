ef4fed490729f49d85b231debac31ee30edbdcc6
rbromfield-ch <rbromfield1@companieshouse.gov.uk>
2025-08-15 15:53:11 +0100

    SIV-537: Consumer Skeleton (#1)
    * SIV-537: Created pom.xml and .gitignore
    
    * SIV-537: Consumer Skeleton
    
    * SIV-537: Add Springboot to pom.xml and removed unneeded classes
    
    * SIV-537: Create Makefile
    
    * SIV-537: Add placeholder test
    
    * SIV0537: Change Java version form 24 to 21
    
    * SIV-537: Consumer Skeleton
    
    * SIV-537: fixing application.properties web pase-path
    
    * SIV-537: adding LoggingUtil and StaticPropertyUtil
    
    * SIV-537: Add PULL_REQUEST_TEMPLATE.md
    
    * SIV-537: Cleaned up tests
    
    * SIV-537: Small update to PULL_REQUEST_TEMPLATE.md
    
    * SIV-537: amending application.properties AuthcodeChangedConsumerApplication and test
    
    # Conflicts:
    #	src/test/java/uk/gov/companieshouse/authcodeChanged/AuthcodeChangedConsumerApplicationTest.java
    
    * SIV-537: removing comments
    
    * SIV-537: Modified pom.xml and Makefile
    
    * SIV-537: Change authcodeChanged to authcode.changed and add application.properties
    
    * SIV-537: Updated .gitignore, pom.xml and moved application.properties
    
    * SIV-537 Updated pom.xml
    
    * SIV-537: application.properties
    
    * SIV-537- Created SecurityConfig.java
    
    * SIV-537: Copied pom.xml from accounts-association-api
    
    * SIV-537: Removed unnecessary dependencies
    
    * SIV-537: Modified SecurityConfig to make healthcheckUrl a global variable
    
    * SIV-537: application.properties test
    
    * SIV-537: Change main class back and update URL in SecurityConfig
    
    * SIV-537: Run formatter on code
    
    * SIV-537: LoggingUtil, SecurityConfig, StaticPropertyUtil
    
    * SIV-537: SecurityConfig, StaticPropertyUtil
    
    ---------
    
    Co-authored-by: rosey-mccloskey-ch <rmccloskey@companieshouse.gov.uk>

---
+++ src/main/java/uk/gov/companieshouse/authcode/changed/AuthcodeChangedConsumerApplication.java
@@ -0,0 +1,21 @@
+package uk.gov.companieshouse.authcode.changed;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import uk.gov.companieshouse.authcode.changed.utils.StaticPropertyUtil;
+
+@SpringBootApplication
+public class AuthcodeChangedConsumerApplication {
+
+    final StaticPropertyUtil staticPropertyUtil;
+
+    @Autowired
+    public AuthcodeChangedConsumerApplication(final StaticPropertyUtil staticPropertyUtil) {
+        this.staticPropertyUtil = staticPropertyUtil;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthcodeChangedConsumerApplication.class, args);
+    }
+}
---
+++ src/main/java/uk/gov/companieshouse/authcode/changed/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package uk.gov.companieshouse.authcode.changed.config;
+
+import static org.springframework.http.HttpMethod.GET;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(
+                request -> request.requestMatchers(GET, "/authcode-changed-consumer/healthcheck").permitAll()
+                        .anyRequest().denyAll())
+                .build();
+    }
+}
