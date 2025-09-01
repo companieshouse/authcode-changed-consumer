1fb04eb376134a74122e79106a6025ebd5bc4753
dhawes-ch <dhawes@companieshouse.gov.uk>
2025-09-01 09:45:17 +0100

    Let API SDK handle fetching API key

--- src/main/java/uk/gov/companieshouse/authcode/changed/config/ApiClientConfig.java
+++ src/main/java/uk/gov/companieshouse/authcode/changed/config/ApiClientConfig.java
@@ -1,19 +1,15 @@
 package uk.gov.companieshouse.authcode.changed.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.companieshouse.api.InternalApiClient;
-import uk.gov.companieshouse.api.http.ApiKeyHttpClient;
+import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
 
 @Configuration
 public class ApiClientConfig {
 
-    @Value("${chs.internal.api.key}")
-    private String internalApiKey;
-
     @Bean
     public InternalApiClient getInternalApiClient() {
-        return new InternalApiClient(new ApiKeyHttpClient(internalApiKey));
+        return ApiSdkManager.getInternalSDK();
     }
 }
